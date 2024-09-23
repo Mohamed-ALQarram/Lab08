d7ac5fb06ec1d62b0d747b43fa12ca05cdb78aa1
Mohamed El-Qarram <mohamed0ibrahim0000@gmail.com>
2024-09-23 23:41:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel.Design;
+using System.Transactions;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter Stack size: ");
+            int.TryParse(Console.ReadLine() ?? "0", out int size);
+            Stack<int> stack = new Stack<int>(size);
+            char ch;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("1.Push");
+                Console.WriteLine("2.Pop");
+                Console.WriteLine("3.Get Element");
+                Console.WriteLine("4.Exit");
+                Console.Write("\nEnter Your Choice: ");
+                char.TryParse(Console.ReadLine() ?? "0", out ch);
+                Console.Clear();
+                switch (ch)
+                {
+                    case '1':
+                        if (stack.TopOfStack == stack.Size - 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Your stack is Over Flow.");
+                            break;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("Enter Pushed Number: ");
+                        int.TryParse(Console.ReadLine() ?? "", out int val);
+                        stack.Push(val);
+                        break;
+                    case '2':
+                        if (stack.TopOfStack == -1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Your stack is Empty");
+                            break;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"\nPoped number is:{stack.Pop()} \n");
+                        break;
+                    case '3':
+                        if (stack.TopOfStack == -1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Your stack is Empty");
+                            break;
+                        }
+                        Console.ForegroundColor= ConsoleColor.Cyan;
+                        Console.Write("Enter Index of Element: ");
+                        int.TryParse(Console.ReadLine(), out int index);
+                        Console.WriteLine(stack.GetByIndex(index));
+                        break;
+                    case '4':
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Enter a Valied Number (1 to 3)");
+                        break;
+                }
+            }
+            while (ch != '4');
+
+
+        } 
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Lab5
+{
+    internal class Stack<T>:GetByIndex<T>
+    {
+        T[] StackArr;
+        public int Size { get; init; }
+        public int TopOfStack {  get; private set; }
+        public Stack() : this(0) { }
+        public Stack(int size)
+        {
+            Size = size;
+            StackArr = new T[Size];
+            TopOfStack = -1;
+        }
+
+        public void Push(T value) 
+        {
+            TopOfStack++;
+            StackArr[TopOfStack] = value;
+            //if (TopOfStack == StackArr.Length-1)
+                //resize();
+        }
+        public T Pop() 
+        {
+        return StackArr[TopOfStack--];
+        }
+
+        public T GetByIndex(int index)
+        {
+            return StackArr[index];
+        }
+
+        public static Stack<T> operator+ (Stack<T> a, Stack<T> b) 
+        {
+            T[] arr = new T[a.Size+b.Size];
+            int top_a = a.TopOfStack;
+            int top_b = b.TopOfStack;
+            while (a.TopOfStack >=0) 
+            {
+                arr[a.TopOfStack] = a.Pop();
+            }
+            while (b.TopOfStack >= 0)
+            {
+                arr[top_a+1+ b.TopOfStack] = b.Pop();
+            }
+            return new Stack<T> { StackArr = arr ,TopOfStack =top_a+top_b+1 , Size = arr.Length};
+        }
+        public T? this[int index]
+        {
+            get 
+            {
+            if(index <StackArr.Length && index >=0) 
+                return StackArr[index];
+            return default;
+            }
+        }
+        public static explicit operator Stack<T>(T[] arr) 
+        {
+            return new Stack<T> { StackArr = arr, TopOfStack = arr.Length-1 };
+        }
+        public static implicit operator T[] (Stack<T> s1)
+        {
+            T[] arr = new T[s1.Size];
+            while (s1.TopOfStack >= 0)
+            {
+                arr[s1 .TopOfStack] = s1.Pop();
+            }
+            return arr;
+        }
+
+    }
+}
